9b634cd007c803f46c82c565789077bc2897fdf8
Walther Chen <walther.chen@gmail.com>
2023-06-04 15:32:16 -0400

    parse dns header

--- src/main.zig
+++ src/main.zig
@@ -22,11 +22,13 @@ pub fn main() !void {
     var out_buf: [1024]u8 = undefined;
     const bytes_read = try std.os.recvfrom(sock, &out_buf, 0, &addr.any, &addr_len);
     const out = out_buf[0..bytes_read];
+    var out_stream = std.io.fixedBufferStream(out);
+    const out_rdr = out_stream.reader();
 
-    std.debug.print("{any}", .{std.fmt.fmtSliceHexLower(out)});
+    std.debug.print("{any}\n", .{std.fmt.fmtSliceHexLower(out)});
+    std.debug.print("{any}\n", .{DnsHeader.from_reader(out_rdr)});
 }
 
-// Returns number of bytes written
 fn writeQuery(id: u16, domain_name: []const u8, record_type: u16, wtr: anytype) !void {
     const header = DnsHeader{
         .id = id,
@@ -59,6 +61,17 @@ const DnsHeader = struct {
         try wtr.writeIntBig(u16, self.num_authorities);
         try wtr.writeIntBig(u16, self.num_additionals);
     }
+
+    fn from_reader(rdr: anytype) !DnsHeader {
+        return DnsHeader{
+            .id = try rdr.readIntBig(u16),
+            .flags = try rdr.readIntBig(u16),
+            .num_questions = try rdr.readIntBig(u16),
+            .num_answers = try rdr.readIntBig(u16),
+            .num_authorities = try rdr.readIntBig(u16),
+            .num_additionals = try rdr.readIntBig(u16),
+        };
+    }
 };
 
 const DnsQuestion = struct {
@@ -80,10 +93,17 @@ const DnsQuestion = struct {
     }
 };
 
-test writeQuery {
+test "write query" {
     var buf = try std.BoundedArray(u8, 1024).init(0);
 
     try writeQuery(0x8298, "www.example.com", TYPE_A, buf.writer());
-    var out_buf: [1024]u8 = undefined;
-    try std.testing.expectEqualSlices(u8, buf.slice(), try std.fmt.hexToBytes(&out_buf, "82980100000100000000000003777777076578616d706c6503636f6d0000010001"));
+    var expected_buf: [1024]u8 = undefined;
+    try std.testing.expectEqualSlices(u8, buf.slice(), try std.fmt.hexToBytes(&expected_buf, "82980100000100000000000003777777076578616d706c6503636f6d0000010001"));
+}
+
+test "parse header" {
+    const response = "`V\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03www\x07example\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00R\x9b\x00\x04]\xb8\xd8";
+    var resp_stream = std.io.fixedBufferStream(response);
+    const actual = try DnsHeader.from_reader(resp_stream.reader());
+    try std.testing.expectEqual(actual, DnsHeader{ .id = 24662, .flags = 33152, .num_questions = 1, .num_answers = 1, .num_authorities = 0, .num_additionals = 0 });
 }
