5247aab869e28922bad29ffc5a219ce2bd2c4d29
Walther Chen <walther.chen@gmail.com>
2023-06-24 16:15:39 -0400

    parse dns question

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,9 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const BoundedArray = std.BoundedArray;
+const fixedBufferStream = std.io.fixedBufferStream;
+const expectEqual = std.testing.expectEqual;
+const expectEqualSlices = std.testing.expectEqualSlices;
 
 const TYPE_A = 1;
 const CLASS_IN = 1;
@@ -85,17 +88,15 @@ const DnsHeader = struct {
     }
 };
 
-test "parse header" {
-    const response = "`V\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03www\x07example\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00R\x9b\x00\x04]\xb8\xd8";
-    var resp_stream = std.io.fixedBufferStream(response);
-    const actual = try DnsHeader.from_reader(resp_stream.reader());
-    try std.testing.expectEqual(actual, DnsHeader{ .id = 24662, .flags = 33152, .num_questions = 1, .num_answers = 1, .num_authorities = 0, .num_additionals = 0 });
-}
-
 const DnsQuestion = struct {
     name: []const u8,
     type: u16,
     class: u16,
+
+    /// pass the same allocator that was used for creation (i.e. using from_reader)
+    fn deinit(self: DnsQuestion, alloc: Allocator) void {
+        alloc.free(self.name);
+    }
 
     fn write_bytes(self: DnsQuestion, wtr: anytype) !void {
         var parts = std.mem.tokenize(u8, self.name, ".");
@@ -132,11 +133,37 @@ const DnsQuestion = struct {
 
         return buf.toOwnedSlice();
     }
+
+    fn from_reader(alloc: Allocator, rdr: anytype) !DnsQuestion {
+        const name = try DnsQuestion.parse_domain_name(alloc, rdr);
+
+        return DnsQuestion{
+            .name = name,
+            .type = try rdr.readIntBig(u16),
+            .class = try rdr.readIntBig(u16),
+        };
+    }
 };
 
+test "parse response" {
+    const alloc = std.testing.allocator;
+    const response = "`V\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x03www\x07example\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00R\x9b\x00\x04]\xb8\xd8";
+    var resp_stream = std.io.fixedBufferStream(response);
+    const resp_rdr = resp_stream.reader();
+
+    try expectEqual(DnsHeader.from_reader(resp_rdr), DnsHeader{ .id = 24662, .flags = 33152, .num_questions = 1, .num_answers = 1, .num_authorities = 0, .num_additionals = 0 });
+
+    const question = try DnsQuestion.from_reader(alloc, resp_rdr);
+    defer question.deinit(alloc);
+    try expectEqualSlices(u8, question.name, "www.example.com");
+    try expectEqual(question.type, 1);
+    try expectEqual(question.class, 1);
+}
+
 test "parse domain name" {
-    var input = std.io.fixedBufferStream("\x03www\x07example\x03com\x00\x00\x01\x00\x01");
-    const actual = try DnsQuestion.parse_domain_name(std.testing.allocator, input.reader());
-    defer std.testing.allocator.free(actual);
-    try std.testing.expectEqualSlices(u8, actual, "www.example.com");
+    const alloc = std.testing.allocator;
+    var input = fixedBufferStream("\x03www\x07example\x03com\x00\x00\x01\x00\x01");
+    const actual = try DnsQuestion.parse_domain_name(alloc, input.reader());
+    defer alloc.free(actual);
+    try expectEqualSlices(u8, actual, "www.example.com");
 }
